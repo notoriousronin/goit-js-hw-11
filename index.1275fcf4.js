2f35cf0413f7663a5aba829eadc6d69f3d3b2cf1
notoriousronin <103944609+notoriousronin@users.noreply.github.com>
2022-09-20 21:20:34 +0000

    Deploying to gh-pages from @ notoriousronin/goit-js-hw-11@fc50caa33f603547c34b7cc7e0ef8367a7208a27 🚀

---
+++ index.1275fcf4.js
@@ -0,0 +1,2 @@
+fetch("https://jsonplaceholder.typicode.com/posts/1",{method:"DELETE"}).then((()=>console.log("Post deleted"))).catch((o=>console.log("Error:",o)));
+//# sourceMappingURL=index.1275fcf4.js.map
