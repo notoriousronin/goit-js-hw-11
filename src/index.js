5e1a4a90d514a04bec0ecde75e54287bbdc83089
Andrey Gavrilyuk <andrew250519@gmail.com>
2022-09-21 23:12:44 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-const postIdToDelete = 1;
+// Change value of id property to update different post
+const postToUpdate = {
+  id: 1,
+  body: 'CRUD is really awesome',
+};
 
-fetch(`https://jsonplaceholder.typicode.com/posts/${postIdToDelete}`, {
-  method: 'DELETE',
-})
-  .then(() => console.log('Post deleted'))
-  .catch(error => console.log('Error:', error));
+const options = {
+  method: 'PATCH',
+  body: JSON.stringify(postToUpdate),
+  headers: {
+    'Content-Type': 'application/json; charset=UTF-8',
+  },
+};
+
+fetch(`https://jsonplaceholder.typicode.com/posts/${postToUpdate.id}`, options)
+  .then(response => response.json())
+  .then(post => console.log(post))
+  .catch(error => console.log('ERROR' + error));
