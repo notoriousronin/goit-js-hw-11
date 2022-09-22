0c3c084ad73d73755736b4b6f6706d94872d17ab
notoriousronin <103944609+notoriousronin@users.noreply.github.com>
2022-09-22 20:09:05 +0000

    Deploying to gh-pages from @ notoriousronin/goit-js-hw-11@2e2c311657855a1d504916bafe8c199f632656b1 🚀

--- index.7f0daf6b.js
+++
@@ -1,2 +0,0 @@
-const o={id:1,body:"CRUD is really awesome"},e={method:"PATCH",body:JSON.stringify(o),headers:{"Content-Type":"application/json; charset=UTF-8"}};fetch(`https://jsonplaceholder.typicode.com/posts/${o.id}`,e).then((o=>o.json())).then((o=>console.log(o))).catch((o=>console.log("ERROR"+o)));
-//# sourceMappingURL=index.7f0daf6b.js.map
