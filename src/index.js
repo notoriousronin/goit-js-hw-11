2e2c311657855a1d504916bafe8c199f632656b1
Andrey Gavrilyuk <andrew250519@gmail.com>
2022-09-22 23:08:30 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,18 +1,2 @@
-// Change value of id property to update different post
-const postToUpdate = {
-  id: 1,
-  body: 'CRUD is really awesome',
-};
-
-const options = {
-  method: 'PATCH',
-  body: JSON.stringify(postToUpdate),
-  headers: {
-    'Content-Type': 'application/json; charset=UTF-8',
-  },
-};
-
-fetch(`https://jsonplaceholder.typicode.com/posts/${postToUpdate.id}`, options)
-  .then(response => response.json())
-  .then(post => console.log(post))
-  .catch(error => console.log('ERROR' + error));
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
